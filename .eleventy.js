b55c68aa8e0f955c6abee9977c6630d8e12a87b6
Alexander King Perocho <perocho.xela@gmail.com>
2025-05-27 11:37:38 -0400

    basic setup

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("js");
+    return {
+        dir: {
+            input: ".",
+            includes: "_includes",
+            layouts: "_layouts",
+            output: "_site"
+        }
+    };
+};
